1da82f7d668490a5d9d8714dc8c8f680161154e2
Aman Prakash Jha <amanjha7151@gmail.com>
2021-03-11 20:27:51 +0530

    build is going on

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from nltk.chat.util import Chat,reflections
 pairs=[
-    ['my name is (.*)',['hello %1']],
+    ['my name is (.*)',['hello %1 , how are you doing today?']],
+    ['bye',['Bye,It was nice conversing with you']],
 ]
-chat=Chat(pairs,reflections)
-chat.converse()
+def chatbot():
+    print("Hi, I am your first chatbot ,pretty basic one!")
+    chat=Chat(pairs,reflections)
+    chat.converse()
+
+if __name__ == '__main__':
+    chatbot()
